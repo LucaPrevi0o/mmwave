3b39c012105245a72214284457d8e028705201f0
LucaPrevi0o <luca01.previati@edu.unife.it>
2025-05-20 09:23:59 +0200

    started toml parser

---
+++ java/toml/TOMLParser.java
@@ -0,0 +1,10 @@
+package java.toml;
+
+public class TOMLParser {
+
+    private static String readFile(String filePath) {
+        
+        // Implement the logic to read the file content
+        return "";
+    }
+}
